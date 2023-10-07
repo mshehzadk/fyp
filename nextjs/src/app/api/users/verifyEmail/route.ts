b29d9151f4a1e7148f0d4562c217ab285dc2c209
Muhammad Shehzad <mshehzadmshehzad113440@gmail.com>
2023-10-08 00:37:59 +0500

    updated route

--- nextjs/src/app/api/users/verifyEmail/route.ts
+++ nextjs/src/app/api/users/verifyEmail/route.ts
@@ -2,6 +2,7 @@ import { connect } from '@/dbConfig/dbConfig';
 import { NextResponse, NextRequest } from 'next/server';
 import User from '@//models/userModel';
 
+connect();
 export async function POST(request: NextRequest) {
     try {
         const reqBody = await request.json();
