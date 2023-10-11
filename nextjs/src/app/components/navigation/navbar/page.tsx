d6ce13f15097af81ad71e172efbb17e6cde259a5
Muhammad Shehzad <mshehzadmshehzad113440@gmail.com>
2023-10-11 13:07:12 +0500

    updated navbar

--- nextjs/src/app/components/navigation/navbar/page.tsx
+++ nextjs/src/app/components/navigation/navbar/page.tsx
@@ -1,5 +1,5 @@
 'use client';
-import { usePathname } from 'next/navigation'
+import { usePathname } from 'next/navigation';
 
 import React, { useEffect, useState } from 'react';
 import Link from 'next/link';
@@ -7,69 +7,92 @@ import Logo from './Logo';
 import Button from './Button';
 
 const Navbar = ({ toggle }: { toggle: () => void }) => {
-    const pathname = usePathname()
+    const pathname = usePathname();
     const [active, setActive] = useState('Home');
-   
+
     useEffect(() => {
-        
         // Set the active link based on the current route
         setActive(pathname);
-        console.log(pathname)
-      }, [pathname]);
+        console.log(pathname);
+    }, [pathname]);
 
-      if(pathname === '/login' || pathname === '/signup'){
-        return <></>
+    if (pathname === '/login' || pathname === '/signup') {
+        return <></>;
     }
 
     return (
-       
         <div>
             <div className="w-full h-20 bg-black sticky top-0 drop-shadow-md">
-                <div className="container mx-auto px-4 h-full">
-                    <div className="flex justify-evenly items-center h-full">
-                    
-                                <div className='flex '>
-                                <Logo />
-                                <h1 className='hidden md:block text-white font-serif text-xl font-bold mt-5 '>Dublingo</h1>
-                                </div>
-                                
-                                <button
-                                    type="button"
-                                    className="inline-flex items-center md:hidden"
-                                    onClick={toggle}
-                                >
-                                    <svg
-                                        xmlns="http://www.w3.org/2000/svg"
-                                        width="40"
-                                        height="40"
-                                        viewBox="0 0 24 24"
+                <div className=" px-4 h-full flex flex-row">
+                    <div className="flex w-[100%] md:w-[20%] ">
+                        <Logo />
+                        <h1 className="hidden md:block text-white font-ubuntu italic text-2xl font-bold mt-5 ">
+                        <span className="text-blue-500">Dub</span>
+                                <span className="text-white">Lingo</span>
+                        </h1>
+                    </div>
+                    <div className="flex justify-end items-center h-full md:w-[80%] w-[100%] ">
+                        <button
+                            type="button"
+                            className="inline-flex items-center md:hidden"
+                            onClick={toggle}
+                        >
+                            <svg
+                                xmlns="http://www.w3.org/2000/svg"
+                                width="40"
+                                height="40"
+                                viewBox="0 0 24 24"
+                            >
+                                <path
+                                    fill="#fff"
+                                    d="M3 6h18v2H3V6m0 5h18v2H3v-2m0 5h18v2H3v-2Z"
+                                />
+                            </svg>
+                        </button>
+                        <ul className="hidden md:flex gap-x-14 text-gray-100 font-mono font-bold   ">
+                            <li>
+                                <Link href="/">
+                                    <p
+                                        className={`${
+                                            active === '/'
+                                                ? 'bg-white text-gray-800 rounded-[18px]'
+                                                : ' nav'
+                                        }    px-6 py-2`}
                                     >
-                                        <path
-                                            fill="#fff"
-                                            d="M3 6h18v2H3V6m0 5h18v2H3v-2m0 5h18v2H3v-2Z"
-                                        />
-                                    </svg>
-                                </button>
-                                <ul className="hidden md:flex gap-x-14 text-gray-100 font-mono font-bold   ">
-                                    <li>
-                                        <Link href="/"  >
-                                            <p className={`${active === '/' ? 'bg-white text-gray-800 rounded-[18px]' : ' nav'}    px-6 py-2`}>Home</p>
-                                        </Link>
-                                    </li>
-                                    <li>
-                                        <Link href="/team">
-                                            <p  className={` ${active === '/team' ? 'bg-white text-gray-800 rounded-[18px]' : 'nav'}  px-6 py-2`}>Team</p>
-                                        </Link>
-                                    </li>
-                                    <li>
-                                        <Link href="/services">
-                                            <p className={`${active === '/services' ? 'bg-white text-gray-800  rounded-[18px]' : ' nav'}  px-6 py-2`}>Services</p>
-                                        </Link>
-                                    </li>
-                                </ul>
-                        
-                        <div className="hidden md:block">
-                            <Button content='Try Now'/>
+                                        Home
+                                    </p>
+                                </Link>
+                            </li>
+                            <li>
+                                <Link href="/team">
+                                    <p
+                                        className={` ${
+                                            active === '/team'
+                                                ? 'bg-white text-gray-800 rounded-[18px]'
+                                                : 'nav'
+                                        }  px-6 py-2`}
+                                    >
+                                        Team
+                                    </p>
+                                </Link>
+                            </li>
+                            <li>
+                                <Link href="/services">
+                                    <p
+                                        className={`${
+                                            active === '/services'
+                                                ? 'bg-white text-gray-800  rounded-[18px]'
+                                                : ' nav'
+                                        }  px-6 py-2`}
+                                    >
+                                        Services
+                                    </p>
+                                </Link>
+                            </li>
+                        </ul>
+
+                        <div className="hidden md:block ml-20 mr-3">
+                            <Button content="Try Now" />
                         </div>
                     </div>
                 </div>
