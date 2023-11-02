c92ad2cc8543d0635ce9f5d87d80e3be8cb8d4cc
Zahid <i200469@nu.edu.pk>
2023-11-03 01:33:34 +0500

    Nav Extra Button Remove

--- nextjs/src/app/components/navigation/navbar/page.tsx
+++ nextjs/src/app/components/navigation/navbar/page.tsx
@@ -32,23 +32,6 @@ const Navbar = ({ toggle }: { toggle: () => void }) => {
                         </h1>
                     </div>
                     <div className="flex justify-end items-center h-full md:w-[80%] w-[100%] ">
-                        <button
-                            type="button"
-                            className="inline-flex items-center md:hidden"
-                            onClick={toggle}
-                        >
-                            <svg
-                                xmlns="http://www.w3.org/2000/svg"
-                                width="40"
-                                height="40"
-                                viewBox="0 0 24 24"
-                            >
-                                <path
-                                    fill="#fff"
-                                    d="M3 6h18v2H3V6m0 5h18v2H3v-2m0 5h18v2H3v-2Z"
-                                />
-                            </svg>
-                        </button>
                         <ul className="hidden md:flex gap-x-14 text-gray-100 font-mono font-bold   ">
                             <li>
                                 <Link href="/">
