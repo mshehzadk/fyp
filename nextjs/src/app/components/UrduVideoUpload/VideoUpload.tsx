bb10f2cd5ca42768219b1c1e603d6f950aadd141
Zahid <i200469@nu.edu.pk>
2023-11-04 03:13:48 +0500

    Update video upload functionality

--- nextjs/src/app/components/UrduVideoUpload/VideoUpload.tsx
+++ nextjs/src/app/components/UrduVideoUpload/VideoUpload.tsx
@@ -1,16 +1,15 @@
-'user client';
-
 import { useState } from "react";
 
-
 export default function VideoUpload() {
-  const [file, setFile] = useState<File>();
+  const [file, setFile] = useState<File | null>(null);
+  const [videoUrl, setVideoUrl] = useState<string | null>(null);
+  const [isUploading, setIsUploading] = useState<boolean>(false);
 
   const onSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault(); // Prevent form refresh
+    if (!file) return alert('No file selected.');
 
-    e.preventDefault();
-    if (!file) return alert('File not exist.')
-    console.log(file)
+    setIsUploading(true); // Set the isUploading state to true when the upload starts
 
     try {
       const data = new FormData();
@@ -19,34 +18,56 @@ export default function VideoUpload() {
       const resForNextApi = await fetch('/api/users/VideoUpload', {
         method: 'POST',
         body: data,
-      })
+      });
       const resquestForFlask = await fetch('http://localhost:8080/uploadUrduVideo', {
         method: 'POST',
         body: data,
       });
-      // Handle the error
-      if (!resForNextApi.ok  && !resquestForFlask.ok) {
+
+      if (!resForNextApi.ok && !resquestForFlask.ok) {
         throw new Error(await resForNextApi.text());
       }
-    }
-    catch (e: any) { // Handle the error
+
+      // Create a local URL for the file to use in the video player
+      setVideoUrl(URL.createObjectURL(file));
+    } catch (e: any) {
       console.log(e.message);
+    } finally {
+      setIsUploading(false); // Set the isUploading state to false when the upload is complete
     }
-  }
+  };
 
-
+  const onFileChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    if (e.target.files?.length) {
+      setFile(e.target.files[0]);
+      setVideoUrl(null); // Clear the previous video URL
+    }
+  };
 
   return (
     <div>
-      <h1>Video Upload</h1>
-      <form onSubmit={onSubmit}>
-        <input type="file"
-          name="file"
-          accept="video/mp4"
-          onChange={(e) => setFile(e.target.files?.[0])}
-        />
-        <input type="submit" value="Upload" />
-      </form>
+      <div>
+        <form onSubmit={onSubmit}>
+          <input type="file"
+            name="file"
+            accept="video/mp4"
+            onChange={onFileChange}
+          />
+          {file && <div style={{ textAlign: 'right' }}>
+            <input type="submit" value="Upload" />
+          </div>}
+        </form>
+      </div>
+      <div>
+        {isUploading ? (
+          <div>Loading...</div> // This is where your spinner would go
+        ) : videoUrl ? (
+          <video controls>
+            <source src={videoUrl} type="video/mp4" />
+            Your browser does not support the video tag.
+          </video>
+        ) : null}
+      </div>
     </div>
-  )
+  );
 }
