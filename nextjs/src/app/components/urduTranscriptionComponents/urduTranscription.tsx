712b814eb1bf62d09c28739b0de37a67a6ab57ca
Zahid <i200469@nu.edu.pk>
2023-11-08 02:01:01 +0500

    UrduTranscription Page started

--- nextjs/src/app/components/urduTranscriptionComponents/urduTranscription.tsx
+++ nextjs/src/app/components/urduTranscriptionComponents/urduTranscription.tsx
@@ -1,19 +1,27 @@
 import { useState } from "react"
 
-import Header  from "../urduTranscriptionComponents/header"
+import Header from "../urduTranscriptionComponents/header"
 
 export default function urduTranscription() {
     return (
         <div className="Transcription Video">
-        <div>
-            <div>
-                <button className="btn">Click Me</button>
-            </div>
-            <div className="Transcriptions">
+            <div className="Transcription">
+                <div>
+                    <button className="btn">Click Me</button>
+                </div>
+                <div className="Transcriptions">
 
+                </div>
+            </div>
+            <div className="Transcription">
+                <div>
+                    <button className="btn">Click Me</button>
+                </div>
+                <div className="Transcriptions">
+
+                </div>
             </div>
         </div>
-        </div>
-                
+
     )
 }
