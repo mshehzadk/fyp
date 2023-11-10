1e477812be063d2bb82ef5521f0f7b5ecb35e589
Zahid <i200469@nu.edu.pk>
2023-11-10 21:04:47 +0500

    daisy disable for now

--- nextjs/tailwind.config.ts
+++ nextjs/tailwind.config.ts
@@ -24,9 +24,10 @@ const config: Config = {
       }
     },
   },
-  plugins: [require('daisyui')],
-  daisyui: {
-    themes: ["light", "dark", "cupcake"],
-  },
+  plugins: [],
+  // plugins: [require('daisyui')],
+  // daisyui: {
+  //   themes: ["light", "dark", "cupcake"],
+  // },
 }
 export default config
