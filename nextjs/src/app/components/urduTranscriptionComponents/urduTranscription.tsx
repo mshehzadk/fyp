4ed2d49ab7b71b70183b3713653d89f83b104938
Zahid <i200469@nu.edu.pk>
2023-11-10 23:15:15 +0500

    'fs' is install

--- nextjs/src/app/components/urduTranscriptionComponents/urduTranscription.tsx
+++ nextjs/src/app/components/urduTranscriptionComponents/urduTranscription.tsx
@@ -1,15 +1,31 @@
 'use Client'
 /* eslint-disable react-hooks/rules-of-hooks */
 import { useRef } from "react";
+import fs from 'fs';
+import path from 'path';
+import { NextRequest } from "next/server";
 
-export default function urduTranscription() {
+export default async function urduTranscription() {
     const videoRef = useRef<HTMLVideoElement>(null);
+    const getTranscription = async () => {
+        // Fetch the JSON data from the Flask server
+        const response = await fetch('http://localhost:5000/api/data');
+        const data = await response.json();
+
+        // Write the JSON data to a file in the public folder
+        const filePath = path.join(process.cwd(), 'public', 'urduTranscription.json');
+        fs.writeFileSync(filePath, JSON.stringify(data));
+        console.log('Data written to file');
+        console.log(data);
+    }
+    // Call the function to get the transcription data
+    getTranscription();
 
     return (
         <div className="Transcription Video" style={{display:'flex'}}>
             <div className="Transcription" style={{flex:'1',textAlign: 'right'}}>
                 <p>Zahid</p>
-                
+
             </div>
             <div className="Video" style={{flex:'1',textAlign: 'right'}}>
                 {( videoRef ? 
