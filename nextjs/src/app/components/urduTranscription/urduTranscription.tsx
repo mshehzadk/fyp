b00024d94a1cdded4ca8d672984f8265854076e0
Zahid <i200469@nu.edu.pk>
2023-11-11 23:13:36 +0500

    Update video and transcription components

--- nextjs/src/app/components/urduTranscription/urduTranscription.tsx
+++ nextjs/src/app/components/urduTranscription/urduTranscription.tsx
@@ -1,59 +1,16 @@
 /* eslint-disable react-hooks/rules-of-hooks */
 import { useEffect, useRef, useState } from "react";
-import { usePathname } from "next/navigation";
 import Urdutranscriptionlist from "./urduTranscriptionList";
 import Headertranscription from "./header";
 
 export default function urduTranscription() {
     const videoRef = useRef<HTMLVideoElement>(null);
-    const [transcription, setTranscription] = useState<JSON>();
-    try {
-        useEffect(() => {
-            fetch('http://localhost:8080/api/urduTranscription').then((response) => {
-                response.json().then((data) => {
-                    setTranscription(data);
-                    console.log(data);
-
-                })
-            })
-
-        }, []);
-    }
-    catch (error: any) {
-        console.log(error);
-    }
-
-   function handleEdit() {
-    console.log("Edit");
-   }
 
     return (
         <div className="Transcription Video" style={{ display: 'flex' }}>
             <div className="Transcription" style={{ flex: '1', textAlign: 'right' }}>
                 <Headertranscription />
                 <Urdutranscriptionlist />
-                <div>
-                    {transcription && Array.isArray(transcription) && transcription.map((item: any, index: number) => (
-                        <div key={index} className="p-4 border border-salte-300 my-3 flex justify-between gap-5">
-                            <div>
-                                <div className="flex justify-between">
-                                    <input className="font-bold text-xl" value={item.speaker} onChange={handleEdit}></input>
-                                    <input className="m-2" value={item.startTime}></input>
-                                    <input className="m-2" value={item.endTime} ></input>
-                                </div>
-                                <div>
-                                    <input value={item.transcription}></input>
-                                </div>
-                            </div>
-                            <div>
-                                <button className="bg-slate-800 text-white px-3 py-2 rounded-md m-2">Edit</button>
-                                <button className="bg-slate-800 text-white px-3 py-2 rounded-md m-2">Delete</button>
-                            </div>
-                        </div>
-                    ))}
-                    {!transcription && <div>Loading......</div>}
-                </div>
-
             </div>
             <div className="Video" style={{ flex: '1', textAlign: 'right' }}>
                 {(videoRef ?
--- nextjs/src/app/components/urduTranscription/urduTranscriptionList.tsx
+++ nextjs/src/app/components/urduTranscription/urduTranscriptionList.tsx
@@ -1,5 +1,51 @@
+/* eslint-disable react-hooks/rules-of-hooks */
+import { useEffect, useRef, useState } from "react";
+import { usePathname } from "next/navigation";
+
 export default function urduTranscriptionList() {
+    const [transcription, setTranscription] = useState<JSON>();
+    try {
+        useEffect(() => {
+            fetch('http://localhost:8080/api/urduTranscription').then((response) => {
+                response.json().then((data) => {
+                    setTranscription(data);
+                    console.log(data);
+
+                })
+            })
+
+        }, []);
+    }
+    catch (error: any) {
+        console.log(error);
+    }
+
+    function handleEdit() {
+        console.log("Edit");
+    }
     return (
-        <h1>Zahid</h1>
+        <div>
+            {transcription && Array.isArray(transcription) && transcription.map((item: any, index: number) => (
+                <div key={index} className="p-4 border border-salte-300 my-3 flex justify-between gap-5">
+                    <div>
+                        <div className="flex justify-between">
+                            <input className="font-bold text-xl" value={item.speaker} onChange={handleEdit}></input>
+                            <input className="m-2" value={item.startTime}></input>
+                            <input className="m-2" value={item.endTime} ></input>
+                        </div>
+                        <div>
+                            <input value={item.transcription}></input>
+                        </div>
+                    </div>
+                    <div>
+                        <button className="bg-slate-800 text-white px-3 py-2 rounded-md m-2">Edit</button>
+                        <button className="bg-slate-800 text-white px-3 py-2 rounded-md m-2">Delete</button>
+                    </div>
+                </div>
+            ))}
+            {!transcription && <div>Loading......</div>}
+        </div>
+
+
     )
 }
