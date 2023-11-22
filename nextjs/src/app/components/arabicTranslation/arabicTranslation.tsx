40d159373b4459a8c5efba6c3a988a8ca2faf3d1
Zahid <i200469@nu.edu.pk>
2023-11-22 23:51:42 +0500

    Delete Arabic and Urdu translation files

--- nextjs/src/app/components/arabicTranslation/arabicTranslation.tsx
+++ nextjs/src/app/components/arabicTranslation/arabicTranslation.tsx
@@ -4,6 +4,7 @@ import Link from "next/link";
 import { AiOutlinePlus } from "react-icons/ai";
 
 export default function arabicTranslation() {
+    const [urduTranscription, setUrduTranscription] = useState<JSON>();
     const [data, setData] = useState<JSON>();
     const [editTrigger, setEditTrigger] = useState(false);
     const [addTranslationModal, setAddTranslationModal] = useState(false);
@@ -19,6 +20,12 @@ export default function arabicTranslation() {
             fetch('http://localhost:8080/api/arabicTranslation').then((response) => {
                 response.json().then((data) => {
                     setData(data);
+                })
+            })
+
+            fetch('http://localhost:8080/api/urduTranscription').then((response) => {
+                response.json().then((data) => {
+                    setUrduTranscription(data);
                 })
             })
 
@@ -188,99 +195,102 @@ export default function arabicTranslation() {
     return (
         <div>
             <div>
-                <>
-                    <div className="flex justify-between items-center bg-slate-800 px-3 py-3">
-                        <p className="text-white font-bold">
-                            Arabic Translation
-                        </p>
-                        <button className="bg-white mx-5 flex" onClick={addTranslation}>
-                            Add Translation <AiOutlinePlus className="ml-2" size={18} />
-                        </button>
-                    </div>
-                    {
-                        addTranslationModal &&
-                        <div>
-                            <div>
-                                <div className="flex"></div>
-                                <input className="font-bold text-xl" placeholder="Enter Speaker Name" value={speakerName} onChange={(e: any) => setSpeakerName(e.target.value)}></input>
-                                <input className="m-2" placeholder="Start Time" value={startTime} onChange={(e: any) => TimeUpdate(e, 's')}></input>
-                                <input className="m-2" placeholder="End Time" value={endTime} onChange={(e: any) => TimeUpdate(e, 'e')}></input>
-                                {!dateFormat && <div className="text-red-500">Please enter a valid time string in the format HH:MM:SS</div>}
-                            </div>
-                            <div className="flex">
-                                <input placeholder="Trnascription" value={translation} onChange={(e: any) => setTranslation(e.target.value)}></input>
-                            </div>
-                            <div>
-                                <button className="bg-slate-800 text-white px-3 py-2 rounded-md m-2" onClick={(e) => SaveNew(e)}>
-                                    Save
-                                </button>
-                                <button className="bg-slate-800 text-white px-3 py-2 rounded-md m-2" onClick={IgnoreChanges}>
-                                    Ingore
-                                </button>
-                            </div>
-                        </div>
-                    }
-                </>
+                <div>
 
-                {data && Array.isArray(data) && data.map((item: any, index: number) => (
-                    <div key={index} className="p-4 border border-slate-300 my-5 flex gap-2">
-                        {editTrigger && editIndex === index ?
+                </div>
+                <div className="">
+                    <>
+                        <div className="flex justify-between items-center bg-slate-800 px-3 py-3">
+                            <p className="text-white font-bold">
+                                Arabic Translation
+                            </p>
+                            <button className="bg-white mx-5 flex" onClick={addTranslation}>
+                                Add Translation <AiOutlinePlus className="ml-2" size={18} />
+                            </button>
+                        </div>
+                        {
+                            addTranslationModal &&
                             <div>
                                 <div>
                                     <div className="flex"></div>
-                                    <input className="font-bold text-xl" value={speakerName} onChange={(e: any) => setSpeakerName(e.target.value)}></input>
-                                    <input className="m-2"
-                                        value={startTime}
-                                        onChange={(e: any) => TimeUpdate(e, 's')}>
-
-                                    </input>
-                                    <input className="m-2"
-                                        value={endTime}
-                                        onChange={(e: any) => TimeUpdate(e, 'e')}>
-
-                                    </input>
+                                    <input className="font-bold text-xl" placeholder="Enter Speaker Name" value={speakerName} onChange={(e: any) => setSpeakerName(e.target.value)}></input>
+                                    <input className="m-2" placeholder="Start Time" value={startTime} onChange={(e: any) => TimeUpdate(e, 's')}></input>
+                                    <input className="m-2" placeholder="End Time" value={endTime} onChange={(e: any) => TimeUpdate(e, 'e')}></input>
                                     {!dateFormat && <div className="text-red-500">Please enter a valid time string in the format HH:MM:SS</div>}
                                 </div>
                                 <div className="flex">
-                                    <input value={translation} onChange={(e: any) => setTranslation(e.target.value)}></input>
+                                    <input placeholder="Trnascription" value={translation} onChange={(e: any) => setTranslation(e.target.value)}></input>
                                 </div>
-                            </div>
-                            :
-                            <div onDoubleClick={() => editTheRow(item.speaker, item.startTime, item.endTime, item.translation, index)}>
-                                <div className="flex">
-                                    <div className="font-bold text-xl">{item.speaker}</div>
-                                    <div className="m-2" >{item.startTime}</div>
-                                    <div className="m-2" >{item.endTime}</div>
-                                </div>
-                                <div className="flex">
-                                    <div>{item.translation}</div>
+                                <div>
+                                    <button className="bg-slate-800 text-white px-3 py-2 rounded-md m-2" onClick={(e) => SaveNew(e)}>
+                                        Save
+                                    </button>
+                                    <button className="bg-slate-800 text-white px-3 py-2 rounded-md m-2" onClick={IgnoreChanges}>
+                                        Ingore
+                                    </button>
                                 </div>
                             </div>
                         }
-                        <div className="flex">
+                    </>
+
+                    {data && Array.isArray(data) && data.map((item: any, index: number) => (
+                        <div key={index} className="p-4 border border-slate-300 my-5 flex gap-2">
                             {editTrigger && editIndex === index ?
+                                <div>
+                                    <div>
+                                        <div className="flex"></div>
+                                        <input className="font-bold text-xl" value={speakerName} onChange={(e: any) => setSpeakerName(e.target.value)}></input>
+                                        <input className="m-2"
+                                            value={startTime}
+                                            onChange={(e: any) => TimeUpdate(e, 's')}>
 
-                                <button className="bg-slate-800 text-white px-3 py-2 rounded-md m-2"
-                                    onClick={(e) => saveEdit(e)}
-                                >Save</button>
+                                        </input>
+                                        <input className="m-2"
+                                            value={endTime}
+                                            onChange={(e: any) => TimeUpdate(e, 'e')}>
+
+                                        </input>
+                                        {!dateFormat && <div className="text-red-500">Please enter a valid time string in the format HH:MM:SS</div>}
+                                    </div>
+                                    <div className="flex">
+                                        <input value={translation} onChange={(e: any) => setTranslation(e.target.value)}></input>
+                                    </div>
+                                </div>
                                 :
-                                <button className="bg-slate-800 text-white px-3 py-2 rounded-md m-2"
-                                    onClick={() => editTheRow(item.speaker, item.startTime, item.endTime, item.translation, index)}
-                                >Edit</button>
+                                <div onDoubleClick={() => editTheRow(item.speaker, item.startTime, item.endTime, item.translation, index)}>
+                                    <div className="flex">
+                                        <div className="font-bold text-xl">{item.speaker}</div>
+                                        <div className="m-2" >{item.startTime}</div>
+                                        <div className="m-2" >{item.endTime}</div>
+                                    </div>
+                                    <div className="flex">
+                                        <div>{item.translation}</div>
+                                    </div>
+                                </div>
                             }
-                            {!(editTrigger) &&
-                                <button className="bg-slate-800 text-white px-3 py-2 rounded-md m-2" onClick={() => DeleteTranslation(index)}>
-                                    Delete
-                                </button>}
+                            <div className="flex">
+                                {editTrigger && editIndex === index ?
 
+                                    <button className="bg-slate-800 text-white px-3 py-2 rounded-md m-2"
+                                        onClick={(e) => saveEdit(e)}
+                                    >Save</button>
+                                    :
+                                    <button className="bg-slate-800 text-white px-3 py-2 rounded-md m-2"
+                                        onClick={() => editTheRow(item.speaker, item.startTime, item.endTime, item.translation, index)}
+                                    >Edit</button>
+                                }
+                                {!(editTrigger) &&
+                                    <button className="bg-slate-800 text-white px-3 py-2 rounded-md m-2" onClick={() => DeleteTranslation(index)}>
+                                        Delete
+                                    </button>}
+
+                            </div>
                         </div>
-                    </div>
-                ))}
+                    ))}
 
-                {!data && <div>Loading......</div>}
+                    {!data && <div>Loading......</div>}
+                </div>
             </div>
-
-
 
             <div style={{ display: 'flex' }}>
                 <Link href='/urduTranscription' style={{ flex: '1', textAlign: 'right' }}>
