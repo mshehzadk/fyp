3616b311f4d2b6f643b55ce24f0eb13ab0c0bbe6
Ali Tajir <alitajir32@gmail.com>
2023-11-23 20:14:04 +0500

    Added more informtion about members

--- nextjs/src/app/team/page.tsx
+++ nextjs/src/app/team/page.tsx
@@ -2,11 +2,11 @@
 import Head from 'next/head';
 
 const teamMembers = [
-  { name: 'Ali Tajir', role: 'Developer', image: '/images/ali.jpg' },
-  { name: 'Zahid Imran', role: 'Developer', image: '/images/zahid.jpg' },
-  { name: 'Muhammad Shehzad', role: 'Developer', image: '/images/shehzad.jpg' },
-  { name: 'Mahreen Alam', role: 'Supervisor', image: '/images/mahreen.jpg' },
-  { name: 'Greyhat', role: 'External Collaborator', image: '/images/greyhat.jpg' },
+  { name: 'Ali Tajir', role: 'Developer', image: '/images/ali.jpg', details: 'Ali is an experienced developer...' },
+  { name: 'Zahid Imran', role: 'Developer', image: '/images/zahid.jpg', details: 'Zahid is a passionate developer...' },
+  { name: 'Muhammad Shehzad', role: 'Developer', image: '/images/shehzad.jpg', details: 'Shehzad is a skilled developer...' },
+  { name: 'Mahreen Alam', role: 'Supervisor', image: '/images/mahreen.jpg', details: 'Mahreen supervises and guides the team...' },
+  { name: 'Greyhat', role: 'External Collaborator', image: '/images/greyhat.jpg', details: 'Greyhat collaborates with us on various projects...' },
 ];
 
 export default function Team() {
@@ -21,7 +21,7 @@ export default function Team() {
 
       <div className="grid grid-cols-1 md:grid-cols-3 gap-12">
         {teamMembers.slice(0, 3).map((member, index) => (
-          <div key={index} className="bg-white p-6 rounded-lg shadow-md">
+          <div key={index} className="relative bg-white p-6 rounded-lg shadow-md overflow-hidden">
             <img
               src={member.image}
               alt={member.name}
@@ -29,13 +29,16 @@ export default function Team() {
             />
             <h2 className="text-xl font-bold mb-2">{member.name}</h2>
             <p className="text-gray-600">{member.role}</p>
+            <div className="opacity-0 hover:opacity-100 transition-opacity duration-300 absolute inset-0 bg-gray-800 bg-opacity-75 p-4 text-white rounded-lg">
+              <p>{member.details}</p>
+            </div>
           </div>
         ))}
       </div>
 
       <div className="grid grid-cols-1 md:grid-cols-3 gap-12 mt-8">
         {teamMembers.slice(3, 5).map((member, index) => (
-          <div key={index} className="bg-white p-6 rounded-lg shadow-md flex flex-col items-center">
+          <div key={index} className="relative bg-white p-6 rounded-lg shadow-md overflow-hidden flex flex-col items-center">
             <img
               src={member.image}
               alt={member.name}
@@ -43,6 +46,9 @@ export default function Team() {
             />
             <h2 className="text-xl font-bold mb-2">{member.name}</h2>
             <p className="text-gray-600">{member.role}</p>
+            <div className="opacity-0 hover:opacity-100 transition-opacity duration-300 absolute inset-0 bg-gray-800 bg-opacity-75 p-4 text-white rounded-lg">
+              <p>{member.details}</p>
+            </div>
           </div>
         ))}
       </div>
