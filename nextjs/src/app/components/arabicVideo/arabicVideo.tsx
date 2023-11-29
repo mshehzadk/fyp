201576cbb36b7e0c3274a6f02b40c761dfef2462
Zahid <i200469@nu.edu.pk>
2023-11-29 11:20:01 +0500

    Refactor Arabic video component UI

--- nextjs/src/app/components/arabicVideo/arabicVideo.tsx
+++ nextjs/src/app/components/arabicVideo/arabicVideo.tsx
@@ -36,29 +36,31 @@ export default function arabicVideo() {
             <div className="w-[80%] h-[70%] bg-base-100 shadow-xl rounded-xl border border-gray-200 mb-12" >
                 {isLoading ?
                     <div className="flex justify-center items-center w-full h-full rounded-2xl shadow-2xl">
-                        <HashLoader color="#007cf4"/>
+                        <HashLoader color="#007cf4" />
                     </div> :
                     <video className="w-full h-full rounded-2xl shadow-2xl" src={videoSrc} controls />
                 }
             </div>
             <div className="flex w-[80%] mt-5">
-    <Link href='/arabicTranslation' className="flex-1 text-right">
-        <div className="bg-slate-500 text-white py-2 px-4 rounded-md text-center hover:bg-blue-400 transition-all duration-300 flex items-center justify-center w-50%">
-            <FaArrowLeft className="mr-2" />
-            Arabic Translation
-        </div>
-    </Link>
-    {!isLoading &&
-        <button
-            type="button"
-            className="bg-slate-500 text-white py-2 px-4 rounded-md text-center hover:bg-blue-400 transition-all duration-300 flex items-center justify-center w-full"
-            onClick={Download}
-        >
-            <IoMdDownload className="inline-block mr-2" />
-            Download Video
-        </button>
-    }
-</div>
+                <Link href='/arabicTranslation' className="flex-1 pr-2">
+                    <div className="bg-slate-500 text-white py-2 px-4 rounded-md text-center hover:bg-blue-400 transition-all duration-300 flex items-center justify-center w-50%">
+                        <FaArrowLeft className="mr-2" />
+                        Arabic Translation
+                    </div>
+                </Link>
+                {!isLoading &&
+                    <button
+                        type="button"
+                        className="flex-1 pl-2 "
+                        onClick={Download}
+                    >
+                        <div className="bg-slate-500 text-white py-2 px-4 rounded-md text-center hover:bg-blue-400 transition-all duration-300 flex items-center justify-center w-full">
+                        <IoMdDownload className="inline-block mr-2" />
+                        Download Video
+                        </div>
+                    </button>
+                }
+            </div>
 
         </div>
 
