f34ffc41f989e58628395918fe9de97fde42e512
Zahid <i200469@nu.edu.pk>
2023-12-12 21:32:38 +0500

    Update Urdu transcription and download file name

--- nextjs/src/app/components/arabicVideo/arabicVideo.tsx
+++ nextjs/src/app/components/arabicVideo/arabicVideo.tsx
@@ -24,7 +24,7 @@ export default function arabicVideo() {
         const a = document.createElement('a');
         a.style.display = 'none';
         a.href = url;
-        a.setAttribute('download', 'test.video');
+        a.setAttribute('download', 'test.mp4');
         document.body.appendChild(a);
         a.click();
         window.URL.revokeObjectURL(url);
