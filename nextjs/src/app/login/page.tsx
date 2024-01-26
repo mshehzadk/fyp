39e50b62d9a4e095868a3224c6c4e85cb47c023d
Ali Tajir <alitajir32@gmail.com>
2024-01-26 12:30:02 +0500

    login and signup page re-designed

--- nextjs/src/app/login/page.tsx
+++ nextjs/src/app/login/page.tsx
@@ -40,10 +40,10 @@ export default function SignUp() {
         }
     };
     return (
-        <div className="flex items-center justify-center min-h-screen bg-green-100 rounded-md border-6 border-green-200">
-            <div className="flex w-[80%] border-4 border-green-200 rounded-md">
+        <div className="flex items-center justify-center min-h-screen bg-green-100 rounded-md border-6 border-green-200 bg-gradient-to-r from-blue-500 to-green-500">
+            <div className="flex w-[65%] border-4 border-green-200 rounded-md">
                 {/* Login Form */}
-                <div className="w-full md:w-1/2 bg-white shadow-md rounded-md p-8 ">
+                <div className="w-full md:w-1/2 bg-gradient-to-r from-red-200 to-blue-200 shadow-md rounded-md p-14 ">
                     <div className="max-w-md space-y-4">
                         <div className="text-center">
                             <h1 className="text-3xl font-extrabold text-indigo-600 mb-4">
@@ -59,7 +59,7 @@ export default function SignUp() {
                                 />
                             </div>
                         </div>
-                        <div className="mx-8 mt-4 bg-cover bg-center rounded-md">
+                        <div className="mx-1 mt-4 bg-cover bg-center rounded-md">
                             <label
                                 htmlFor="email"
                                 className="block text-sm font-medium text-gray-600"
@@ -81,7 +81,7 @@ export default function SignUp() {
                                 required
                             />
                         </div>
-                        <div className="mx-8 mt-4 bg-cover bg-center rounded-md">
+                        <div className="mx-1 mt-4 bg-cover bg-center rounded-md">
                             <label
                                 htmlFor="password"
                                 className="block text-sm font-medium text-gray-600"
@@ -103,18 +103,18 @@ export default function SignUp() {
                                 required
                             />
                         </div>
-                        <div className="mx-8 mt-8">
+                        <div className="mx-1 mt-8">
                             <button
                                 onClick={onLogin}
-                                className="w-full px-4 py-4 mt-3 text-white bg-indigo-500 rounded-md hover:bg-indigo-600 focus:outline-none focus:ring focus:border-indigo-700 transform transition-transform duration-300 hover:scale-105 border border-red-700"
+                                className="w-full px-5 py-3 mt-3 text-white bg-indigo-500 rounded-md hover:bg-indigo-600 focus:outline-none focus:ring focus:border-indigo-700 transform transition-transform duration-300 hover:scale-105 border border-red-700"
                             >
                                 {loading ? 'Processing' : 'Login'}
                             </button>
                         </div>
-                        <div className="mx-8 mt-8 mb-12">
+                        <div className="mx-1 mt-12 mb-8">
                             <button
                                 onClick={onSignup}
-                                className="w-full px-4 py-4 mt-2 mb-5 text-white bg-gray-400 rounded-md hover:bg-gray-500 focus:outline-none focus:ring focus:border-gray-600 transform transition-transform duration-300 hover:scale-105 border border-red-700"
+                                className="w-full px-5 py-3 mt-2 mb-5 text-white bg-gray-400 rounded-md hover:bg-gray-500 focus:outline-none focus:ring focus:border-gray-600 transform transition-transform duration-300 hover:scale-105 border border-red-700"
                             >
                                 Sign Up
                             </button>
