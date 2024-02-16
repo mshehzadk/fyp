24c67a835b76ca4db5a8fbebd45b133d7596e105
Zahid <i200469@nu.edu.pk>
2024-02-16 22:53:08 +0500

    ali styling added to SignUp

--- nextjs/src/app/signup/page.tsx
+++ nextjs/src/app/signup/page.tsx
@@ -52,22 +52,22 @@ export default function SignUp() {
     };
 
     return (
-        <div className="flex items-center justify-center min-h-screen  rounded-md border-6 border-green-200 bg-gradient-to-r from-blue-500 to-green-500">
-            <div className="flex w-[65%] border-4 border-green-200 rounded-md">
+        <div className="flex items-center justify-center min-h-screen  rounded-md border-6 border-black bg-gradient-to-r from-blue-900 to-black">
+            <div className="flex w-[65%] border-4 border-white rounded-md">
                 {/* Sign Up Form */}
 
 
-                <div className="bg-gradient-to-r from-red-200 to-blue-200 w-full md:w-1/2 rounded-md p-6">
+                <div className="bg-gradient-to-r from-cyan-600 to-cyan-200  w-full md:w-1/2 rounded-md p-6">
                     <div className="max-w-md space-y-4">
                         <div className="text-center">
-                            <h1 className="text-3xl font-extrabold text-indigo-600 mb-4">
+                            <h1 className="text-3xl font-extrabold text-indigo-900 mb-4">
                                 Create your account
                             </h1>
                         </div>
                         <div className="m-6 flex items-center justify-center">
-                            <div className="w-32 h-32 overflow-hidden rounded-full border-4 border-indigo-100">
+                            <div className="w-32 h-32 overflow-hidden rounded-full border-4 border-indigo-500">
                                 <img
-                                    src="https://e0.pxfuel.com/wallpapers/667/98/desktop-wallpaper-login-background-outlook-login.jpg"
+                                    src="https://png.pngitem.com/pimgs/s/235-2350720_security-png-transparent-png.png"
                                     alt="User"
                                     className="object-cover w-full h-full"
                                 />
@@ -76,7 +76,7 @@ export default function SignUp() {
                         <div className="mx-8 mt-2">
                             <label
                                 htmlFor="username"
-                                className="block text-sm font-medium text-gray-600"
+                                className="block text-sm font-medium text-black"
                             >
                                 Username
                             </label>
@@ -91,14 +91,14 @@ export default function SignUp() {
                                         username: e.target.value,
                                     })
                                 }
-                                className="w-full px-3 py-2 mt-2 bg-gray-200 border border-gray-300 rounded-md focus:outline-none focus:ring focus:border-indigo-500"
+                                className="w-full px-3 py-2 mt-2 bg-gray-200 border border-gray-500 rounded-md focus:outline-none focus:ring focus:border-indigo-500"
                                 required
                             />
                         </div>
                         <div className="mx-8 mt-2">
                             <label
                                 htmlFor="email"
-                                className="block text-sm font-medium text-gray-600"
+                                className="block text-sm font-medium text-black"
                             >
                                 Email
                             </label>
@@ -113,14 +113,14 @@ export default function SignUp() {
                                         email: e.target.value,
                                     })
                                 }
-                                className="w-full px-3 py-2 mt-2 bg-gray-200 border border-gray-300 rounded-md focus:outline-none focus:ring focus:border-indigo-500"
+                                className="w-full px-3 py-2 mt-2 bg-gray-200 border border-gray-500 rounded-md focus:outline-none focus:ring focus:border-indigo-500"
                                 required
                             />
                         </div>
                         <div className="mx-8 mt-2">
                             <label
                                 htmlFor="password"
-                                className="block text-sm font-medium text-gray-600"
+                                className="block text-sm font-medium text-black"
                             >
                                 Password
                             </label>
@@ -135,14 +135,14 @@ export default function SignUp() {
                                         password: e.target.value,
                                     })
                                 }
-                                className="w-full px-3 py-2 mt-2 bg-gray-200 border border-gray-300 rounded-md focus:outline-none focus:ring focus:border-indigo-500"
+                                className="w-full px-3 py-2 mt-2 bg-gray-200 border border-gray-500 rounded-md focus:outline-none focus:ring focus:border-indigo-500"
                                 required
                             />
                         </div>
                         <div className="mx-8 mt-8">
                             <button
                                 onClick={onSignUp}
-                                className="w-full px-4 py-3 mt-3 text-white bg-indigo-500 rounded-md hover:bg-indigo-600 focus:outline-none focus:ring focus:border-indigo-700 transform transition-transform duration-300 hover:scale-105 border border-red-700"
+                                className="w-full px-4 py-3 mt-3 text-white bg-indigo-500 rounded-md hover:bg-indigo-600 focus:outline-none focus:ring focus:border-indigo-700 transform transition-transform duration-300 hover:scale-105 border border-white"
                             >
                                 {loading ? 'Processing' : 'Sign Up'}
                             </button>
@@ -150,14 +150,14 @@ export default function SignUp() {
                         <div className="mx-8 mt-3 mb-0">
                             <button
                                 onClick={onLogin}
-                                className="w-full px-4 py-3 mt-2 mb-5 text-white bg-gray-400 rounded-md hover:bg-gray-500 focus:outline-none focus:ring focus:border-gray-600 transform transition-transform duration-300 hover:scale-105 border border-red-700"
+                                className="w-full px-4 py-3 mt-2 mb-5 text-white bg-gray-600 rounded-md hover:bg-gray-500 focus:outline-none focus:ring focus:border-gray-600 transform transition-transform duration-300 hover:scale-105 border border-white"
                             >
                                 Login
                             </button>
                         </div>
                     </div>
                 </div>
-                <div className="hidden md:flex md:w-1/2 bg-cover bg-center rounded-md" style={{ backgroundImage: 'url("https://e0.pxfuel.com/wallpapers/667/98/desktop-wallpaper-login-background-outlook-login.jpg")' }}>
+                <div className="hidden md:flex md:w-1/2 bg-cover bg-center rounded-md" style={{ backgroundImage: 'url("https://png.pngtree.com/background/20210710/original/pngtree-blue-technology-future-artificial-intelligence-science-picture-image_1010975.jpg")' }}>
                 </div>
             </div>
         </div>
