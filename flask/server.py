c70a4eb8d054ef1b3b19ed7a0bf11a2672bac5b2
Zahid <i200469@nu.edu.pk>
2024-03-29 10:36:44 +0500

    Update generate_targetVideo function in flask/server.py

--- flask/server.py
+++ flask/server.py
@@ -428,23 +428,23 @@ def update_Translation():
 # Generate Arabic Video
 @app.route('/generateTargetVideo',methods=['GET'])
 def generate_targetVideo():
-    condition=False
-    if dl.check_path_exist(output_dir+copy_target_json_filename):
-        condition=dl.compare_json_files(output_dir+target_json_filename,output_dir+copy_target_json_filename)
-        if condition:
-            dl.delete_all_generated_files(output_dir,[source_wav_music_filename,copy_source_wav_music_filename,source_wav_vocals_filename])
-    else:
-        dl.copy_json_file(output_dir+target_json_filename,output_dir+copy_target_json_filename)
-    if (not dl.check_path_exist(output_video_path) and dl.check_path_exist(output_dir+target_json_filename) and dl.check_path_exist(video_path)) or condition:
+    # condition=False
+    # if dl.check_path_exist(output_dir+copy_target_json_filename):
+    #     condition=dl.compare_json_files(output_dir+target_json_filename,output_dir+copy_target_json_filename)
+    #     if condition:
+    #         dl.delete_all_generated_files(output_dir,[source_wav_music_filename,copy_source_wav_music_filename,source_wav_vocals_filename])
+    # else:
+    #     dl.copy_json_file(output_dir+target_json_filename,output_dir+copy_target_json_filename)
+    if not dl.check_path_exist(output_video_path): # and dl.check_path_exist(output_dir+target_json_filename) and dl.check_path_exist(video_path)) or condition:
         args=[voice_clone_url,target_json_filename,video_path,output_dir,output_video_path,source_wav_music_filename,source_wav_vocals_filename,copy_target_json_filename]
         # separate music and vocals and transcribe vocals
         my_process = multiprocessing.Process(target=dl.process_arabic_video, args=args)
         # Start the process
         my_process.start()
         return 'Video generation is in progress', 200
-    elif not dl.check_path_exist(output_dir+target_json_filename) or not dl.check_path_exist(output_dir+source_json_filename) or not dl.check_path_exist(video_path):
-        print('Video not found or Transcription not found or Translation not found')
-        return 'Error', 400
+    # elif not dl.check_path_exist(output_dir+target_json_filename) or not dl.check_path_exist(output_dir+source_json_filename) or not dl.check_path_exist(video_path):
+    #     print('Video not found or Transcription not found or Translation not found')
+    #     return 'Error', 400
     else:
         print('Video already exists')
         return 'Video already exists', 200
